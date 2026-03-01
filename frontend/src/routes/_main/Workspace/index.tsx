5b3c48da6eacf7e3ae28e66f80ca5859e5f5969f
Quan Gan <gq12121@gmail.com>
2026-02-28 23:12:00 -0500

    feat(ui): add react-resizable-panels

--- frontend/src/routes/_main/Workspace/index.tsx
+++ frontend/src/routes/_main/Workspace/index.tsx
@@ -1,18 +1,21 @@
 import Stack from '@mui/material/Stack'
 import { ConverterSidebar } from './components/ConverterSidebar'
 import { ConverterWorkspace } from './components/ConverterWorkspace'
+import { Group, Panel, Separator } from 'react-resizable-panels'
 import type { FC } from 'react'
-import styles from './index.module.scss'
 
 const WorkSpace: FC = () => {
   return (
     <Stack direction="row" spacing={2} width="100%" height="100%">
-      <aside className={styles.aside}>
-        <ConverterSidebar />
-      </aside>
-      <section className={styles.section}>
-        <ConverterWorkspace />
-      </section>
+      <Group>
+        <Panel minSize={320} defaultSize="25%" maxSize="50%">
+          <ConverterSidebar />
+        </Panel>
+        <Separator />
+        <Panel>
+          <ConverterWorkspace />
+        </Panel>
+      </Group>
     </Stack>
   )
 }
